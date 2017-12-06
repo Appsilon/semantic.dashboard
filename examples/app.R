2fe576d6d9c23aecafb3492c45914c0264c61717
Michał <michal.maj@appsilon.pl>
2017-12-06 10:52:07 +0100

    uimenu_item and uiicon aliases

--- examples/app.R
+++ examples/app.R
@@ -9,8 +9,8 @@ if(interactive()){
   ui <- dashboardPage(
     dashboardHeader(color = "black"),
     dashboardSidebar(side = "left", size = "thin", color = "black", type = "labeled icon",
-                     uimenu_item("plot_tab", label = "My plot", icon = uiicon("home")),
-                     uimenu_item("table_tab", label = "My table", icon = uiicon("smile"))),
+                     menuItem("plot_tab", label = "My plot", icon = icon("home")),
+                     menuItem("table_tab", label = "My table", icon = icon("smile"))),
     dashboardBody(uitab(id = "plot_tab", active = TRUE,
                         selectInput(inputId =  "variable1", choices = names(mtcars),
                                     label = "Select first variable", selected = "mpg"),
