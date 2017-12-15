176631a68c7b77417f882d43f5b28870ecd7eade
Michał <michal.maj@appsilon.pl>
2017-12-15 12:19:05 +0100

    Chenged unit test.

--- R/constants.R
+++ R/constants.R
@@ -9,15 +9,6 @@ COLUMN_WIDTHS <-   c("", "two", "three", "four", "five", "six", "seven", "eight"
 MIN_COLUMN_WIDTH <- 1
 MAX_COLUMN_WIDTH <- 16
 
-verify_value_allowed <- function(variable, values){
-  var <- get(variable, envir = parent.frame())
-  if (!(var %in% values)){
-    warning(paste(paste0("'", variable, "'"),
-                  "argument should be one of:",
-                  paste0("'", values, "'", collapse = ", ")))
-  }
-}
-
 sidebar_js <- "$('.sidebar.menu .item').tab({onVisible: function() {$(window).resize()} });
      $('.wrapper .ui.sidebar')
   .sidebar({
--- tests/testthat.R
+++ tests/testthat.R
@@ -2,12 +2,3 @@ library(testthat)
 library(semantic.dashboard)
 
 test_check("semantic.dashboard")
-
-for (col in paste0(semantic.dashboard:::ALLOWED_COLORS, "x")) {
-  color <- col
-  expect_success(expect_warning(semantic.dashboard:::verify_value_allowed("color", semantic.dashboard:::ALLOWED_COLORS)))
-}
-for (col in semantic.dashboard:::ALLOWED_COLORS) {
-  color <- col
-  expect_failure(expect_warning(semantic.dashboard:::verify_value_allowed("color", semantic.dashboard:::ALLOWED_COLORS)))
-}
