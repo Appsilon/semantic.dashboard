bcf1d6ae56bffaa1cbb7dd3194d238bf9d97ab56
Michał <michal.maj@appsilon.pl>
2017-12-15 11:46:49 +0100

    Test fix.

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,9 +5,9 @@ test_check("semantic.dashboard")
 
 for (col in paste0(semantic.dashboard:::ALLOWED_COLORS, "x")) {
   color <- col
-  expect_success(expect_warning(verify_value_allowed("color", semantic.dashboard:::ALLOWED_COLORS)))
+  expect_success(expect_warning(semantic.dashboard:::verify_value_allowed("color", semantic.dashboard:::ALLOWED_COLORS)))
 }
 for (col in semantic.dashboard:::ALLOWED_COLORS) {
   color <- col
-  expect_failure(expect_warning(verify_value_allowed("color", semantic.dashboard:::ALLOWED_COLORS)))
+  expect_failure(expect_warning(semantic.dashboard:::verify_value_allowed("color", semantic.dashboard:::ALLOWED_COLORS)))
 }
