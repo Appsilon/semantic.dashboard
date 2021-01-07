c444ef77351c92f925718cf166365f870ee069b8
drzamich <drzamich@gmail.com>
2021-01-07 16:50:30 +0100

    Add SASS to the project. * Add basic structure of SASS files. * Move existing CSS to new SASS files. * Create a script for generating CSS out of SASS.

---
+++ styles/generate_css.R
@@ -0,0 +1,6 @@
+sass::sass(
+  sass::sass_file("styles/main.scss"),
+  options = sass::sass_options(output_style = "compressed"),
+  output = "inst/semantic.dashboard.css",
+  cache_options = sass::sass_cache_options(FALSE)
+)
