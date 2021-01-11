f5db3bfbee91472d20879d9b18913793b145a0a6
drzamich <drzamich@gmail.com>
2021-01-11 09:19:56 +0100

    Generate expanded CSS next to minified version.

--- styles/generate_css.R
+++ styles/generate_css.R
@@ -4,3 +4,10 @@ sass::sass(
   output = "inst/semantic.dashboard.min.css",
   cache_options = sass::sass_cache_options(FALSE)
 )
+
+sass::sass(
+  sass::sass_file("styles/main.scss"),
+  options = sass::sass_options(output_style = "expanded"),
+  output = "inst/semantic.dashboard.css",
+  cache_options = sass::sass_cache_options(FALSE)
+)
