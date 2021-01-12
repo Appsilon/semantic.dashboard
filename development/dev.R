ce4f7ffc13eb10b6dc354d80e0078463a0a3f489
drzamich <drzamich@gmail.com>
2021-01-12 19:42:38 +0100

    Add example apps to dev.R

--- development/dev.R
+++ development/dev.R
@@ -1,4 +1,12 @@
 APP_TO_RUN <- "examples"
+# APP_TO_RUN <- "examples/bus"
+# APP_TO_RUN <- "examples/crandash"
+# APP_TO_RUN <- "examples/sales"
+# APP_TO_RUN <- "examples/minimal/app_sidebar_options.R"
+# APP_TO_RUN <- "examples/minimal/app_sidebar_condition_panel.R"
+# APP_TO_RUN <- "examples/minimal/app_no_sidebar.R"
+# APP_TO_RUN <- "examples/minimal/app_no_header.R"
+# APP_TO_RUN <- "examples/themes/app-darktheme.R"
 
 # Generate CSS
 source("styles/generate_css.R")
