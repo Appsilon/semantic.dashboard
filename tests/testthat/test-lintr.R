9daf13aed729e309c7c562c5cd1f80307b99cc20
dokato <dominik@appsilon.com>
2021-01-14 16:12:36 +0000

    Update test-lintr.R

--- tests/testthat/test-lintr.R
+++ tests/testthat/test-lintr.R
@@ -3,7 +3,7 @@ context("lintr")
 test_that("lintr", {
   print(getwd())
   lintr::expect_lint_free(
-    path = "../../00_pkg_src/shiny.semantic",
+    path = "../../00_pkg_src/semantic.dashboard",
     relative_path = TRUE,
     lintr::line_length_linter(150)
   )
