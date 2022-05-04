a4530ba55ed6d563b2389e1c4cdf78623ca9a9b5
osenan <oriol.senan@praenoscere.com>
2022-05-04 22:07:38 +0200

    Feature: Include validate_session_object to valide session

--- R/utils.R
+++ R/utils.R
@@ -23,3 +23,18 @@ interlace_dividers <- function(elements) {
 random_id_generator <- function(values = 0:9, id_length = 30) {
   paste0(sample(values, id_length, TRUE), collapse = "")
 }
+
+# Check that an object is a ShinySession object, and give an informative error.
+# The default label is the caller function's name.
+validate_session_object <- function(session, label = as.character(sys.call(sys.parent())[[1]])) {
+  if (missing(session) ||
+      !inherits(session, c("ShinySession", "MockShinySession", "session_proxy")))
+  {
+    stop(call. = FALSE,
+         sprintf(
+           "`session` must be a 'ShinySession' object. Did you forget to pass `session` to `%s()`?",
+           label
+         )
+    )
+  }
+}
